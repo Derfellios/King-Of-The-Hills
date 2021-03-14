74b4a81196e6847feb44723eea441fa6967891d1
Derfellios <joris.nieuwveld@gmail.com>
2021-03-14 22:14:58 +0100

    Add files via upload

--- Client_PresentSettingsUI.lua
+++ Client_PresentSettingsUI.lua
@@ -2,5 +2,5 @@
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
     UI.CreateLabel(vert).SetText('Number of hills in the game: ' .. Mod.Settings.NumberOfHills);
 	UI.CreateLabel(vert).SetText('Number of armies on each hill: ' .. Mod.Settings.SizeOfHills);
-	UI.CreateLabel(vert).SetText('Capture all hills and hold them to until the end of a turn to win.');
+	UI.CreateLabel(vert).SetText('Capture all hills and hold them until the end of a turn to win.');
 end
