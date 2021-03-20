7a60333a28c81ee2ab96c758f3c91a8cf89581f7
Derfellios <joris.nieuwveld@gmail.com>
2021-03-20 21:45:42 +0100

    Update Client_PresentSettingsUI.lua

--- Client_PresentSettingsUI.lua
+++ Client_PresentSettingsUI.lua
@@ -1,6 +1,6 @@
  function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	UI.CreateLabel(vert).SetText('The hills can found by clicking on 'Game' on the bottom left and then on 'Mod: King Of The Hill');
+	UI.CreateLabel(vert).SetText("The hills can found by clicking on 'Game' on the bottom left and then on 'Mod: King Of The Hill");
     	UI.CreateLabel(vert).SetText('Number of hills in the game: ' .. Mod.Settings.NumberOfHills);
 	UI.CreateLabel(vert).SetText('Number of armies on each hill: ' .. Mod.Settings.SizeOfHills);
 	UI.CreateLabel(vert).SetText('Hold all hills at the end of a turn to win!');
